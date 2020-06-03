9cff8d58b2e4088b1b20954299e4d0ca2ba3c48e
Shadi Shafighi <dsh.shadi@gmail.com>
2020-06-03 22:23:33 +0200

    Update cactus_parallel_samples.sh

--- code/cactus_parallel_samples.sh
+++ code/cactus_parallel_samples.sh
@@ -5,7 +5,7 @@ declare -a arr=("S12118" "S144")
 for i in "${arr[@]}"
 do
    echo $i
-   Rscript cactus_run.R  "../input/WES/wes.rds"  "../input/scRNA/ac.rds"  "../results/"   "../tree/"  $i "../input/scBCR_GEX.rds"  "0.5" "9.5" &
+   Rscript cactus_run.R  $1  $2  $3  $4  $i $5 &
 done
 wait
 echo "All done"
